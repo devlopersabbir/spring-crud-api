828d0d593e1d87d4ca05373d46b23a2e18046fdc
devlopersabbir <devlopersabbir@gmail.com>
2023-09-12 22:05:39 +0600

    Product Repository

---
+++ src/main/java/com/devlopersabbir/crud/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.devlopersabbir.crud.repository;
+import com.devlopersabbir.crud.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
